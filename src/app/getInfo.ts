2baaa4da870bf4deef85e7d937f73328726d7ebd
Jonathan Bangert <jonathan@bangert.dk>
2024-10-08 23:57:18 -0600

    Idk man

--- src/app/getInfo.ts
+++ src/app/getInfo.ts
@@ -1,6 +1,11 @@
 "use server";
 
 export async function getInfo(barcode: string) {
+    const raw_data = await fetch("https://world.openfoodfacts.org/api/v0/product/" + barcode + ".json");
+    const json_data = await raw_data.json();
 
-    return { message: "Hello, World!" + barcode };
+    const keywords = json_data.product._keywords;
+    const image_url = json_data.product.image_url;
+
+    return { keywords, image_url };
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,19 @@ import { getInfo } from "./getInfo";
 
 export default function Home() {
   const [data, setData] = useState("");
+  const [info, setInfo] = useState({ keywords: [], image_url: "" });
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <BarcodeScannerComponent
         width={400}
         height={400}
-        onUpdate={(err, result) => {
+        onUpdate={async (err, result) => {
           if (result) {
             setData(result.getText());
-            const info = getInfo(data);
+            const info = await getInfo(data);
             console.log(info);
+            setInfo(info);
           }
         }}
       />
@@ -24,6 +26,8 @@ export default function Home() {
         <h1 className="text-2xl font-bold">QR Code Scanner</h1>
         <p className="text-lg">Scanning...</p>
         <p className="text-lg">{data}</p>
+        <p className="text-lg">{info.keywords.join(", ")}</p>
+        <img src={info.image_url} alt="Product" className="w-48 h-48" />
       </div>
     </div>
   );
