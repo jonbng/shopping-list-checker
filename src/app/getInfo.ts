9e5dc5bddde43df9a75c6035fed1e3a73db4fae1
Jonathan Bangert <jonathan@bangert.dk>
2024-10-09 00:10:53 -0600

    Idk man

--- src/app/getInfo.ts
+++ src/app/getInfo.ts
@@ -2,10 +2,20 @@
 
 export async function getInfo(barcode: string) {
     const raw_data = await fetch("https://world.openfoodfacts.org/api/v0/product/" + barcode + ".json");
+    if (!raw_data.ok) {
+        return { keywords: [], image_url: "" };
+    }
     const json_data = await raw_data.json();
 
+    if (!json_data.product) {
+        return { keywords: [], image_url: "" };
+    }
     const keywords = json_data.product._keywords;
     const image_url = json_data.product.image_url;
+
+    if (!keywords || !image_url) {
+        return { keywords: [], image_url: "" };
+    }
 
     return { keywords, image_url };
 }
