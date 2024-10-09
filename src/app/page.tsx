e977fcfd1940713e6ea9e3e1f7831c184d848b62
Jonathan Bangert <jonathan@bangert.dk>
2024-10-08 23:37:45 -0600

    Add stuff

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState } from "react";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
+
+export default function Home() {
+  const [data, setData] = useState("");
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <BarcodeScannerComponent
+        width={400}
+        height={400}
+        onUpdate={(err, result) => {
+          if (result) setData(result.getText());
+        }}
+      />
+      <div className="text-center">
+        <h1 className="text-2xl font-bold">QR Code Scanner</h1>
+        <p className="text-lg">Scanning...</p>
+        <p className="text-lg">{data}</p>
+      </div>
+    </div>
+  );
+}
