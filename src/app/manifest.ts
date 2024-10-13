15ecc6d64c0b4b5cfc6de6553ff7b9e418f7c819
Jonathan Bangert <jonathan@bangert.dk>
2024-10-13 17:40:46 -0600

    Fix logo

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,9 +2,10 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Shopping List Checker",
+    name: "ScanShop",
     short_name: "ScanShop",
-    description: "An app that checks items off your shopping list when you scan them",
+    description:
+      "An app that checks items off your shopping list when you scan them",
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
@@ -14,6 +15,15 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/favicon.png",
         sizes: "100x100",
         type: "image/png",
+      },
+      {
+        src: "/favicon.svg",
+        type: "image/svg+xml",
+      },
+      {
+        src: "/favicon.ico",
+        sizes: "64x64 32x32 24x24 16x16",
+        type: "image/x-icon",
       }
     ],
   };
