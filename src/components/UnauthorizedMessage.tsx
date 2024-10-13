93477446d244895462da9098ce63c7a35bff87ff
Jonathan Bangert <jonathan@bangert.dk>
2024-10-13 17:19:56 -0600

    Fix logo

--- src/components/UnauthorizedMessage.tsx
+++ src/components/UnauthorizedMessage.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { handleLogin } from "@/msal/msal";
