52e9e3f5f879f08e7f0027892df54ecbf7858908
Jonathan Bangert <jonathan@bangert.dk>
2025-02-26 18:23:05 -0700

    Opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import MyMsalProvider from "@/msal/MyMsalProvider";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -12,7 +12,11 @@ const geistSans = localFont({
 
 export const metadata: Metadata = {
   title: "ShopScan",
-  description: "An app that checks items off your shopping list when you scan them",
+  description:
+    "An app that checks items off your shopping list when you scan them",
+  openGraph: {
+    image: "/og-image.png",
+  },
 };
 
 export default function RootLayout({
@@ -23,11 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.className} bg-gray-100 flex flex-col p-2`}>
-      <Analytics />  
-      <MyMsalProvider>
-          <main>
-            {children}
-          </main>
+        <Analytics />
+        <MyMsalProvider>
+          <main>{children}</main>
         </MyMsalProvider>
       </body>
     </html>
